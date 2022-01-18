3fc034c334965c9988850ba6250d4ffb123371d1
Dirk Stolle <striezel-dev@web.de>
2022-01-18 21:16:45 +0100

    initial commit

---
+++ msi-info/msi-info/MsiUtils.cs
@@ -0,0 +1,93 @@
+﻿/*
+    This file is part of msi-info.
+    Copyright (C) 2022  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace msi_info
+{
+    /// <summary>
+    /// Utility class that wraps calls to msi.dll functions to get MSI property data.
+    /// </summary>
+    internal static class MsiUtils
+    {
+        [DllImport("msi.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        private static extern uint MsiOpenPackageW(string szPackagePath, out IntPtr hProduct);
+
+        [DllImport("msi.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        private static extern uint MsiCloseHandle(IntPtr hAny);
+        
+        [DllImport("msi.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        private static extern uint MsiGetPropertyW(IntPtr hAny, string name, StringBuilder buffer, ref int bufferLength);
+
+        /// <summary>
+        /// Gets a property from an MSI file.
+        /// </summary>
+        /// <param name="msiPath">path to the MSI file</param>
+        /// <param name="property">name of the property (case-sensitive)</param>
+        /// <returns>Returns the value of the property in case of success.
+        /// Returns null, if an error occurred.</returns>
+        private static string GetProperty(string msiPath, string property)
+        {
+            IntPtr MsiHandle = IntPtr.Zero;
+            try
+            {
+                var res = MsiOpenPackageW(msiPath, out MsiHandle);
+                if (res != 0)
+                {
+                    return null;
+                }
+                int length = 512;
+                var buffer = new StringBuilder(length);
+                res = MsiGetPropertyW(MsiHandle, property, buffer, ref length);
+                return buffer.ToString();
+            }
+            finally
+            {
+                if (MsiHandle != IntPtr.Zero)
+                {
+                    _ = MsiCloseHandle(MsiHandle);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the product code of the MSI.
+        /// The product code is an GUID in the form of {12345678-9ABC-DEF0-1234-56789ABCDEF0}.
+        /// </summary>
+        /// <param name="msiPath">path to the MSI file</param>
+        /// <returns>Returns the product code in case of success.
+        /// Returns null, if an error occurred.</returns>
+        public static string GetProductCode(string msiPath)
+        {
+            return GetProperty(msiPath, "ProductCode");
+        }
+
+        /// <summary>
+        /// Gets the product name of the MSI.
+        /// </summary>
+        /// <param name="msiPath">path to the MSI file</param>
+        /// <returns>Returns the product name in case of success.
+        /// Returns null, if an error occurred.</returns>
+        public static string GetProductName(string msiPath)
+        {
+            return GetProperty(msiPath, "ProductName");
+        }
+    }
+}
---
+++ msi-info/msi-info/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+    This file is part of msi-info.
+    Copyright (C) 2022  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.IO;
+
+namespace msi_info
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("msi-info");
+
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Error: You have to specify the path to an MSI file as first parameter!");
+                return 1;
+            }
+
+            string msiPath = args[0];
+            if (!File.Exists(msiPath))
+            {
+                Console.Error.WriteLine("Error: The file " + msiPath + " does not exist.");
+                return 2;
+            }
+
+            var name = MsiUtils.GetProductName(msiPath);
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.Error.WriteLine("Error: Could not get MSI property from file!");
+                return 3;
+            }
+            var code = MsiUtils.GetProductCode(msiPath);
+            if (string.IsNullOrEmpty(code))
+            {
+                Console.Error.WriteLine("Error: Could not get MSI product code from file!");
+                return 3;
+            }
+
+            Console.WriteLine("Product name: " + name);
+            Console.WriteLine("Product code: " + code);
+            return 0;
+        }
+    }
+}
