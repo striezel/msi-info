42b129ee4d252241d44359c86bcb28304b5a9cb2
Dirk Stolle <striezel-dev@web.de>
2024-11-08 06:21:26 +0100

    add version of .NET runtime to version output

--- msi-info/msi-info/Program.cs
+++ msi-info/msi-info/Program.cs
@@ -32,6 +32,8 @@ namespace msi_info
             var ver = asm.GetName().Version;
             Console.WriteLine(asm.GetName().Name + ", version " + ver.ToString(3));
             Console.WriteLine();
+            Console.WriteLine(".NET runtime: " + System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+            Console.WriteLine();
             Console.WriteLine("Copyright (C) 2022 - 2024  Dirk Stolle");
             Console.WriteLine("License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>");
             Console.WriteLine("This is free software: you are free to change and redistribute it under the");
